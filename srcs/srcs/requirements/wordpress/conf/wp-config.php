26274f883795760a1f71f1bd9f903ad260607f5f
jgim <jgim@student.42seoul.kr>
2022-04-05 19:42:15 +0900

    word

--- srcs/srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/srcs/requirements/wordpress/conf/wp-config.php
@@ -12,14 +12,14 @@ define( 'DB_CHARSET', 'utf8' );
 
 define( 'DB_COLLATE', '' );
 
-define('AUTH_KEY',         'hCr^%g1Q24s7nxc9qQ8kX1Q:Xo/ii%%@EQI0%6!aU* !aH<]%U+&4MmjzSs,8JUU');
-define('SECURE_AUTH_KEY',  'ix$7C (kU1SE_0r5t^ha+YVP|mxiD&$-TYp:7M-!CrL[ n@jmkZF$>Jw!;P4K(V[');
-define('LOGGED_IN_KEY',    'Nb[Azs:HR-w]6mn:2V>;V<.{.}9%2ey>Ke0qDSt?K}nm/ ):T|]bv&4|Q<iD6FIE');
-define('NONCE_KEY',        ';+6e=x#<8;Iwg,<eP0L7-$f>moDL#$6_Y]2-vB+nKYOEK_ar@J<a]a5WLvqT5o7A');
-define('AUTH_SALT',        '+Ksn=xq7p>V`zd$`GA2jZa%05uCqe>AkR*MDd}jI{Q}Mr2wk@s,zmUnx/|V-1[U+');
-define('SECURE_AUTH_SALT', '2e9?R>%Q8rlt8oJ?GXbo/u~92[0=MPOTK..5PC+M!OG#78P[ERci &uE~m[b^)5!');
-define('LOGGED_IN_SALT',   '8E!T2ql[gfg1E.`#2|S_7u_ff{UkG)7T cI&+#rWKqjcKo^LPh$/%A:o9.CW[%(e');
-define('NONCE_SALT',       'DyVqx.[+w}toRLdFy!!4qpo1uLSpZY4{g#E[Wl}MIf?u43e4MwhZjK2w9TL|AGNU');
+define( 'AUTH_KEY',         'fiea[c+O+7#fzAceF)|uPY]D)lZJ3Di12;7;l6c2+<ET)(355Z(MVRZ]6u3OKu87' );
+define( 'SECURE_AUTH_KEY',  '}W_{|fcQb{(VE;mTyXGE80W-Mvkc)4|qE91+6$H2w),;^9]oC]hY|v>ItE-UuQ[;' );
+define( 'LOGGED_IN_KEY',    'j2ADH=Am7dx_-RyACrr;A9P(@!-w}SL5N+O1TJU1 n/)x^%e5DIH-1f/(fH5|aJG' );
+define( 'NONCE_KEY',        'j}Ci`C%0m}m=KcQhD@)Y9dIJ2ASJY5Yi6L]q&=,nf/QM7;m9QMm{*rx}n+Yu$Of9' );
+define( 'AUTH_SALT',        'dfl=.0q CepI.JJ ;(A@&G+cJj;f/<XS0W-8*6G1i8~)PZ2y[_[ko:|N1+AQDM1|' );
+define( 'SECURE_AUTH_SALT', '-Y=L( V;&h*|1/+*g^XdByBMvHbGczU+lW MX!X5N)r3lp?Vb*+gE+HQjZS},S_q' );
+define( 'LOGGED_IN_SALT',   '0KtC]!%]8?4}>Kb^?CN-^/-*1v[Ybm/i <#UhXQIerxmNFm:`GA{sQ^Ta)l[MkWv' );
+define( 'NONCE_SALT',       'S|b4anU25E4+|nT:_&_Qa4BI-`|d)@;$BFrCF?a .Ze#^+C2+{EO>Kzs?2vT(7;8' );
 
 $table_prefix = 'wp_';
 
@@ -28,4 +28,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', __DIR__ . '/' );
 }
 
-require_once (ABSPATH . 'wp-settings.php');
+require_once ABSPATH . 'wp-settings.php';
