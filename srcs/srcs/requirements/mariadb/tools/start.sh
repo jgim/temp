ef1a5e0c1674fe9a56858da590068a2c9e90e9e3
jgim <jgim@student.42seoul.kr>
2022-04-06 10:21:06 +0900

    test

--- srcs/srcs/requirements/mariadb/tools/start.sh
+++ srcs/srcs/requirements/mariadb/tools/start.sh
@@ -1,19 +1,40 @@
+# mysql_install_db --user=mysql --datadir=/var/lib/mysql
+# #mysql_install_db는 mysqld를 사용할 준비가 되기 전에 수행해야하는 초기화 작업
+# #mySQL 데이터 디렉토리를 초기화하고 여기에 포함된 시스템 테이블을 생성
+# #datadir : 데이터 디렉토리 경로 = var/lib/mysql
+
+# mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress;"
+
+# mysql -u root -e "CREATE USER 'jgim'@'%' IDENTIFIED BY 'jgim';"
+# mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'jgim'@'%';"
+
+# mysql -u root -e "CREATE USER 'jgim'@'localhost' IDENTIFIED BY 'jgim';"
+# mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'jgim'@'localhost';"
+
+# mysql -u root -e "FLUSH PRIVILEGES;"
+
+# mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'jgim';";
+
+
+# /usr/bin/mysqld -u root
+
+# Set up WordPress Database
 mysql_install_db --user=mysql --datadir=/var/lib/mysql
-#mysql_install_db는 mysqld를 사용할 준비가 되기 전에 수행해야하는 초기화 작업
-#mySQL 데이터 디렉토리를 초기화하고 여기에 포함된 시스템 테이블을 생성
-#datadir : 데이터 디렉토리 경로 = var/lib/mysql
+/usr/bin/mysqld -u root & sleep 1
 
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress;"
+# 새 사용자를 만들고 사용자에게 wp-db에 모든 권한을 부여
+mysql -u root -e "CREATE USER 'silim'@'%' IDENTIFIED BY 'silim';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'silim'@'%';"
 
-mysql -u root -e "CREATE USER 'jgim'@'%' IDENTIFIED BY 'jgim';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'jgim'@'%';"
+mysql -u root -e "CREATE USER 'silim'@'localhost' IDENTIFIED BY 'silim';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'silim'@'localhost';"
 
-mysql -u root -e "CREATE USER 'jgim'@'localhost' IDENTIFIED BY 'jgim';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'jgim'@'localhost';"
+mysql wordpress -u root < ./wordpress.sql
 
-mysql -u root -e "FLUSH PRIVILEGES;"
+mysql -u root -e "FLUSH PRIVILEGES;"  # 변경 사항 저장 및 활성화
 
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'jgim';";
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'silim';";
 
-
+pkill mysqld
 /usr/bin/mysqld -u root
