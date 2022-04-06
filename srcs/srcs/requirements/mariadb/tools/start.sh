1720293cb5cd8495812807b33116d250effb1fc1
jgim <jgim@student.42seoul.kr>
2022-04-06 10:19:39 +0900

    test

--- srcs/srcs/requirements/mariadb/tools/start.sh
+++ srcs/srcs/requirements/mariadb/tools/start.sh
@@ -3,8 +3,6 @@ mysql_install_db --user=mysql --datadir=/var/lib/mysql
 #mySQL 데이터 디렉토리를 초기화하고 여기에 포함된 시스템 테이블을 생성
 #datadir : 데이터 디렉토리 경로 = var/lib/mysql
 
-/usr/bin/mysqld -u root & sleep 1
-
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress;"
 
 mysql -u root -e "CREATE USER 'jgim'@'%' IDENTIFIED BY 'jgim';"
@@ -15,8 +13,7 @@ mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'jgim'@'localhost';"
 
 mysql -u root -e "FLUSH PRIVILEGES;"
 
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'jgim;";
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'jgim';";
 
-pkill mysqld
 
 /usr/bin/mysqld -u root
