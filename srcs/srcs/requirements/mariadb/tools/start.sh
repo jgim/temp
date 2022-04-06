5fd6297a2a96bb471b360e786adeaa59b6b602eb
jgim <jgim@student.42seoul.kr>
2022-04-06 10:23:18 +0900

    test

--- srcs/srcs/requirements/mariadb/tools/start.sh
+++ srcs/srcs/requirements/mariadb/tools/start.sh
@@ -24,17 +24,17 @@ mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress;"
 # 새 사용자를 만들고 사용자에게 wp-db에 모든 권한을 부여
-mysql -u root -e "CREATE USER 'silim'@'%' IDENTIFIED BY 'silim';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'silim'@'%';"
+mysql -u root -e "CREATE USER 'jgim'@'%' IDENTIFIED BY 'jgim';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'jgim'@'%';"
 
-mysql -u root -e "CREATE USER 'silim'@'localhost' IDENTIFIED BY 'silim';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'silim'@'localhost';"
+mysql -u root -e "CREATE USER 'jgim'@'localhost' IDENTIFIED BY 'jgim';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'jgim'@'localhost';"
 
 mysql wordpress -u root < ./wordpress.sql
 
 mysql -u root -e "FLUSH PRIVILEGES;"  # 변경 사항 저장 및 활성화
 
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'silim';";
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'jgim';";
 
 pkill mysqld
 /usr/bin/mysqld -u root
