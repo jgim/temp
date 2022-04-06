0c80f3800680ca3dbbfce997b31eef4e7dbedd45
jgim <jgim@student.42seoul.kr>
2022-04-06 11:33:12 +0900

    Test

--- srcs/srcs/requirements/mariadb/tools/start.sh
+++ srcs/srcs/requirements/mariadb/tools/start.sh
@@ -17,4 +17,5 @@ mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'jgim'@'localhost';"
 mysql -u root -e "FLUSH PRIVILEGES;"
 mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'jgim';";
 
+pkill mysqld
 /usr/bin/mysqld -u root
