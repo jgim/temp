a2f892b00d1310caf4fa7ec134329f3ba433f574
jgim <jgim@student.42seoul.kr>
2022-04-06 09:36:48 +0900

    test

--- srcs/srcs/requirements/mariadb/tools/start.sh
+++ srcs/srcs/requirements/mariadb/tools/start.sh
@@ -6,10 +6,10 @@ mysql_install_db --user=mysql --datadir=/var/lib/mysql
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress;"
 
 mysql -u root -e "CREATE USER 'jgim'@'%' IDENTIFIED BY 'jgim';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'jgim';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'jgim'%';"
 
 mysql -u root -e "CREATE USER 'jgim'@'localhost' IDENTIFIED BY 'jgim';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'jgim';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'jgim'@'localhost';"
 
 mysql -u root -e "FLUSH PRIVILEGES;"
 
